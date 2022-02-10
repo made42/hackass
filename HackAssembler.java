c5689c556d846f1f9da50bfd483bb0b11a580ebe
Maarten Derks <made@freedom.nl>
2022-02-10 17:03:30 +0100

    Add main class and method

---
+++ HackAssembler.java
@@ -0,0 +1,14 @@
+/**
+ * This is the main program that drives the entire assembly process, using the services of the Parser and Code modules.
+ * The program gets the name of the input source file, say, <i>Prog</i>, from the command-line argument. It constructs
+ * a Parser for parsing the input file <i>Prog</i><code>.asm</code> and creates an output file,
+ * <i>Prog</i><code>.hack</code>, into which it will write the translated binary instructions.
+ *
+ * @author Maarten Derks
+ */
+class HackAssembler {
+
+    public static void main(String[] args) {
+
+    }
+}
